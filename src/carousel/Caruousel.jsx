27d0a1fed0f1c2a58d6bf2b23d726289483ff3f4
Grechkin Danil <grechkin-danil@mail.ru>
2023-04-02 03:20:46 +0300

    all ready

--- src/carousel/Caruousel.jsx
+++ src/carousel/Caruousel.jsx
@@ -4,12 +4,17 @@ import styles from './index.module.css';
 
 const Carousel = ({ children }) => {
   const [pages, setPages] = useState([]);
+  const [offset, setOffset] = useState(0);
 
   const handleLeftClick = () => {
-    console.log('handleLeftClick');
+    const newOffset = offset + 100;
+    setOffset((prevState) => (prevState = Math.min(newOffset, 0)));
   };
   const handleRightClick = () => {
-    console.log('handleRightClick');
+    const newOffset = offset - 100;
+    const maxOffset = -(100 * (pages.length - 1));
+    console.log(maxOffset);
+    setOffset((prevState) => (prevState = Math.max(newOffset, maxOffset)));
   };
 
   useEffect(() => {
@@ -29,7 +34,9 @@ const Carousel = ({ children }) => {
     <div className={styles.mainContainer}>
       <FaChevronLeft className={styles.arrow} onClick={handleLeftClick} />
       <div className={styles.window}>
-        <div className={styles.allPagesContainer}>{pages}</div>
+        <div className={styles.allPagesContainer} style={{ transform: `translateX(${offset}%)` }}>
+          {pages}
+        </div>
       </div>
       <FaChevronRight className={styles.arrow} onClick={handleRightClick} />
     </div>
